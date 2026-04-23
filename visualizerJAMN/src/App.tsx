1ab083860f626a5fb4cf499239917e5e086df707
Jose Angel Martinez Navarro <josekun13@gmail.com>
2026-04-23 15:30:09 -0600

    change PointCloudReader Component

--- visualizerJAMN/src/App.tsx
+++ visualizerJAMN/src/App.tsx
@@ -1,25 +1,17 @@
 import { APITester } from "./APITester";
 import "./index.css";
-import "./components/pointCloudReader"
+import "./components/PointCloudReader"
 
 import logo from "./logo.svg";
 import reactLogo from "./react.svg";
-import PointCloudLoader from "./components/pointCloudReader";
+import { PointCloudReader } from "./components/PointCloudReader";
 
 export function App() {
-  var res = PointCloudLoader();
   return (
     <div className="app">
-      <div className="logo-container">
-        <img src={logo} alt="Bun Logo" className="logo bun-logo" />
-        <img src={reactLogo} alt="React Logo" className="logo react-logo" />
-      </div>
 
-      <h1>Bun + React</h1>
-      <p>
-        Edit <code>src/App.tsx</code> and save to test HMR
-      </p>
-      <APITester />
+      <PointCloudReader ></PointCloudReader>
+
     </div>
   );
 
---
+++ visualizerJAMN/src/components/PointCloudReader.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const PointCloudReader = () => {
+    return (
+        <div>PointCloudReader</div>
+    )
+}
+
+
